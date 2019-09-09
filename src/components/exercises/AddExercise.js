3aa44258b5bbcb579a535d41984416203d26609d
Alex Finnarn <alex.finnarn@gmail.com>
2019-09-09 10:33:08 -0400

    replace autocomplete with picker in AddExercise component

--- src/components/exercises/AddExercise.js
+++ src/components/exercises/AddExercise.js
@@ -1,60 +1,32 @@
 import React, {useState, useEffect} from 'react';
-import {Text, View, TouchableOpacity, Keyboard, Switch, TextInput, Button, FlatList} from "react-native";
+import {Text, View, TouchableOpacity, Keyboard, Switch, TextInput, Button, FlatList, Picker} from "react-native";
 import Autocomplete from 'react-native-autocomplete-input';
 import shortId from 'shortid';
 import {styles} from "../Styles";
 import AddExerciseSet from "./AddExerciseSet";
 
 export default function AddExercise({exercises, thing, handleUpdate, addExercise, navigation, theExercise}) {
-  console.log(theExercise);
-
   const [query, setQuery] = useState(theExercise.name);
-  const [hideResults, setHideResults] = useState(false);
   const [exercise, setExercise] = useState(theExercise);
   const [switchValue, setSwitchValue] = useState(true);
-  const exerciseList = findExercise(query);
-  const comp = (a, b) => a.toLowerCase().trim() === b.toLowerCase().trim();
+  const [pickerEnabled, setPickerEnabled] = useState(theExercise.name === '');
 
-  function findExercise(query) {
-    if (query === '') {
-      return [];
-    }
-
-    const regex = new RegExp(`${query.trim()}`, 'i');
-    return exercises.filter(exercise => exercise.name.search(regex) >= 0);
-  }
-
-  function updateAutocompleteInput(name) {
+  function updateExercisePicker(name) {
     setQuery(name);
     setExercise(exercises.find(exercise => exercise.name.toLowerCase().trim() === name.toLowerCase().trim()));
-    setHideResults(true);
-    Keyboard.dismiss();
-  }
-
-  function changeTheText(text) {
-    setQuery(text);
-    setHideResults(false);
   }
 
   return (
     <View style={{flex: 1, flexDirection: 'column'}}>
       <View style={{flex: 1, flexDirection: 'row'}}>
         {switchValue
-          ? <Autocomplete
-            autoCapitalize="none"
-            autoCorrect={false}
-            containerStyle={styles.autocompleteContainer}
-            hideResults={hideResults}
-            data={exerciseList.length === 1 && comp(query, exerciseList[0].name) ? [] : exerciseList}
-            defaultValue={query}
-            onChangeText={text => changeTheText(text)}
-            placeholder="Enter Exercise"
-            renderItem={({item: {name}}) => (
-              <TouchableOpacity style={{paddingTop: 10, paddingBottom: 10}} key={shortId.generate()} onPress={() => updateAutocompleteInput(name)}>
-                <Text style={styles.itemText}>{name}</Text>
-              </TouchableOpacity>
-            )}
-          />
+          ? <Picker
+            enabled={pickerEnabled}
+            selectedValue={query}
+            style={{height: 60, width: 120}}
+            onValueChange={(itemValue) => updateExercisePicker(itemValue)}>
+            {exercises.map((exer, index) => <Picker.Item key={exer.key} label={exer.name} value={exer.name}/>)}
+          </Picker>
           : <TextInput
             style={[styles.editText, styles.smallTextInputFont, {flex: 4}]}
             placeholder="Enter Exercise"
@@ -81,7 +53,7 @@ export default function AddExercise({exercises, thing, handleUpdate, addExercise
         <Button onPress={() => {
           addExercise(exercise);
           navigation.goBack();
-        }} title="Add" />
+        }} title="Add"/>
       </View>
     </View>
   );
