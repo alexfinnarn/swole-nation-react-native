82a66a6da74268ec888d01596320b46cc89aa51f
Alex Finnarn <alex.finnarn@gmail.com>
2019-09-12 15:12:03 -0400

    consolidate remaining list item components

--- src/components/workouts/WorkoutsList.js
+++ src/components/workouts/WorkoutsList.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import {FlatList, View} from "react-native";
-import WorkoutListItem from "./WorkoutListItem";
-import {styles} from '../Styles';
+import {FlatList, Text, View} from "react-native";
+import {home, styles} from '../Styles';
 import ActionButton from "../utility/ActionButton";
 
 export default function WorkoutsList({ workouts, navigation }) {
@@ -11,12 +10,24 @@ export default function WorkoutsList({ workouts, navigation }) {
     return workout;
   });
 
+  function WorkoutsListItem({workout}) {
+    return (
+      <View style={home.sectionContainer}>
+        <View style={home.sectionLeft}>
+          <Text style={home.sectionHeaderText}>{workout.item.name}</Text>
+          <Text style={{flex: 4, marginTop: 15}}>Other content</Text>
+        </View>
+        <ActionButton text="Edit" action={() => navigation.navigate('Workout', {workoutId: workout.item.id})}/>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <View style={{flex:9}}>
         <FlatList
           data={workouts}
-          renderItem={(item) => <WorkoutListItem workout={item} navigation={navigation}/>}
+          renderItem={(item) => <WorkoutsListItem workout={item} />}
         />
       </View>
       <ActionButton text="Add Workout" action={() => navigation.navigate('Workout')} />
