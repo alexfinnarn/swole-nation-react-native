72b693d7e3a4b66fb537c699f61e6aa2f050d080
Alex Finnarn <alex.finnarn@gmail.com>
2019-09-20 12:49:57 -0400

    add tests for sessions list

--- src/components/sessions/SessionsList.js
+++ src/components/sessions/SessionsList.js
@@ -4,11 +4,12 @@ import {home, styles} from '../Styles';
 import ActionButton from "../utility/ActionButton";
 import ActionCard from "../utility/ActionCard";
 
-export default function WorkoutsList({ sessions, navigation }) {
+export default function SessionsList({ sessions, navigation, handle, thing }) {
   return (
-    <View style={styles.container}>
+    <View style={styles.container} testID="sessions-list-root">
       <View style={{flex:9}}>
         <FlatList
+          extraData={thing}
           data={sessions}
           renderItem={(item) => <SessionsListItem session={item}/>}
         />
@@ -20,7 +21,10 @@ export default function WorkoutsList({ sessions, navigation }) {
   function SessionsListItem({session}) {
     return (
       <ActionCard actionComponent={
-        <ActionButton text="Edit" action={() => navigation.navigate('Session', {sessionId: session.item.key})}/>
+        <ActionButton text="Edit" action={() => {
+          handle.setActiveSessionKey(session.item.key);
+          navigation.navigate('Session', {sessionId: session.item.key})
+        }}/>
       }>
         <Text style={home.sectionHeaderText}>{session.item.name}</Text>
         <Text style={{flex: 4, marginTop: 15}}>Other content</Text>
--- src/store/main.js
+++ src/store/main.js
@@ -26,13 +26,15 @@ function mainStore(state = data, action) {
     case WorkoutActions.SET_ACTIVE_WORKOUT:
       return Object.assign({}, state, {
         activeWorkoutKey: action.key,
-        activeTransformerKey: action.transformerKey ?? 'none'});
+        activeTransformerKey: action.transformerKey ?? 'none'
+      });
 
     case WorkoutActions.CREATE_EXERCISE:
       exercise = {key: shortId.generate(), new: true, name: '', instructions: '', sets: []};
       return Object.assign({}, state, {
         exercises: {...state.exercises, [exercise.key]: exercise},
-        activeExerciseKey: exercise.key});
+        activeExerciseKey: exercise.key
+      });
 
     case WorkoutActions.UPDATE_EXERCISE:
       return Object.assign({}, state, {
@@ -105,6 +107,9 @@ function mainStore(state = data, action) {
         sessions: {...state.sessions, [session.key]: session},
         activeSessionKey: session.key
       });
+
+    case WorkoutActions.SET_ACTIVE_SESSION:
+      return Object.assign({}, state, {activeSessionKey: action.key});
 
     case WorkoutActions.FINISH_SESSION:
       return Object.assign({}, state, {sessions: {...state.sessions, [action.session.key]: action.session}});
