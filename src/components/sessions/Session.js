25379f4cf12fa2eeb711267f0dddfff1a489a953
Alex Finnarn <alex.finnarn@gmail.com>
2019-09-20 20:50:34 -0400

    try fixing missing module error

--- src/components/sessions/Session.js
+++ src/components/sessions/Session.js
@@ -3,7 +3,7 @@ import {Text, View, Button, Image} from "react-native";
 import {styles} from "../Styles";
 import Table from "../utility/Table";
 import ActionButton from "../utility/ActionButton";
-import backgroundTimer from '../../services/BackgroundTimer';
+import backgroundTimer from '../../services/BackgroundTimer.js';
 
 export default function Session({session, navigation, handle}) {
   const [set, updateSet] = useState(0);
